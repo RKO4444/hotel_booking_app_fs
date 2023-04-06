94171ccfd52ab911107da65e898dbddad052306a
AmitPratapMallcrm <123299538+AmitPratapMallcrm@users.noreply.github.com>
2023-04-06 16:32:02 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+//this id for apm
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
